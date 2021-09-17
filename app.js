73a53f599341b248482657f961af9bd99e49cefc
jozoshu <mj93m@naver.com>
2021-09-18 00:22:46 +0900

    feat: 메인 홈화면 렌더링

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import apieRouter from './src/api/router';
+import homeRouter from './src/home/router';
 
 const app = express();
 
 // route
-app.get('/', (req, res) => {
-    res.send('Search My Job!')
-});
-app.use('/api', apieRouter)
+app.get('/', homeRouter);
+app.use('/api', apieRouter);
 
 export default app;
--- src/api/config/settings.js
+++ src/api/config/settings.js
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 export default {
+    BASE_DIR: path.join(__dirname, '../../..'),
     ENV: process.env.ENV,
     DB_HOST: process.env.DB_HOST,
     DB_NAME: process.env.DB_NAME,
