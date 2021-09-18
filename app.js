72f49f6c9bccbb117e799215ddce2c1d18f90293
jozoshu <mj93m@naver.com>
2021-09-18 16:09:23 +0900

    feat: 리액트 프로젝트 등록

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import path from 'path';
 import apieRouter from './src/api/router';
 import homeRouter from './src/home/router';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname + '/react/build')));
+
 // route
 app.get('/', homeRouter);
 app.use('/api', apieRouter);
+app.use('/home', (req, res, next) => {
+    res.sendFile(path.join(__dirname + '/react/build/index.html'));
+});
 
 export default app;
